67ec7444f70f22c7796a7543f73d1a8dab626049
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-27 14:03:37 -0300

    feat: add components Navbar é Icons svg, metadados para page Home, estruturação html layout inicial, novo favicon, e icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -5,7 +6,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "App Faturamento",
-  description: "Aplicativo web faturamentto",
+  description: "Aplicativo web faturamento",
+  keywords: ["Faturamento", "Contas", "Boletos", "Fatura"],
+  icons: {
+    icon: "/logo.svg",
+    shortcut: "/logo.svg",
+    apple: "/logo.svg",
+  },
+  openGraph: {
+    title: "App Faturamento",
+    description: "",
+    url: "/",
+    type: "website",
+    siteName: "App Faturamento",
+    locale: "pt-br",
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +30,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import Logo from "../Icons/Logo";
+
+export default function Navbar() {
+  return (
+    <aside>
+      <div>
+        <Logo />
+      </div>
+      <div>
+        <button type="button">
+                  
+        </button>
+      </div>
+    </aside>
+  );
+}
