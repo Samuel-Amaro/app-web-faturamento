5a3bef2bb492ae950daba8b8f57329aa1d8806f9
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-29 17:59:44 -0300

    featt: criando componente de icon svg ArrowDown

---
+++ src/components/Icons/ArrowDown.tsx
@@ -0,0 +1,13 @@
+export default function ArrowDown() {
+  return (
+    <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 1l4.228 4.228L9.456 1"
+        stroke="#7C5DFA"
+        strokeWidth="2"
+        fill="none"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
