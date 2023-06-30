6f263ed5c468d3ab4193c6a4ce93f635a33025ec
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-30 15:06:48 -0300

    feat: reestruturação da function onChange

--- src/components/ContentPageHome/index.tsx
+++ src/components/ContentPageHome/index.tsx
@@ -5,19 +5,22 @@ import { useDatasContext } from "@/context/DatasContext";
 import DropdownFilter from "../DropdownFilter";
 
 export default function ContentPageHome() {
-  const [filters, setFilters] = useState<string[]>([]);
   const datasContext = useDatasContext();
+  const [filters, setFilters] = useState<string[]>([]);
   const optionsFilter = [
     { value: "pago", label: "Pago" },
     { value: "pendente", label: "Pendente" },
     { value: "rascunho", label: "Rascunho" },
   ];
 
-  function onChange(value: string) {
-    //TODO: criar um state de string[] para armazenar as options de filter escolhidas
+  function onChange(value: string, typeAction: "add" | "delete") {
     console.log(`valor escolhido dropdown: ${value}`);
-    if (filters.indexOf(value) === -1) {
-      filters.push(value);
+    if (typeAction === "add") {
+      if (filters.indexOf(value) === -1) {
+        setFilters([...filters, value]);
+      }
+    } else {
+      setFilters(filters.filter((f) => f === value));
     }
   }
 
@@ -28,11 +31,7 @@ export default function ContentPageHome() {
           <h1>Faturas</h1>
           <span>{datasContext.datas.length} faturas</span>
         </div>
-        <DropdownFilter
-          options={optionsFilter}
-          onChange={onChange}
-          filtersSelecteds={filters}
-        />
+        <DropdownFilter options={optionsFilter} onChange={onChange} />
       </header>
     </>
   );
