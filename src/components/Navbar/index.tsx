6bf025332db830056a49a5579c68d5fcc17844df
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-01 18:50:59 -0300

    feat: add styles mobile-first Navbar componente

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import Logo from "../Icons/Logo";
 import Switch from "../Switch";
+import styles from "./styles.module.css";
 
 export default function Navbar() {
   return (
-    <aside>
-      <div>
+    <aside className={styles.container}>
+      <Logo className={styles.containerLogo} />
+      {/*<div className={styles.containerWrapperLogo}>
         <Logo />
-      </div>
-      <div>
-        <Switch />
-        <hr />
+  </div>*/}
+      <div className={styles.containerControls}>
+        <Switch className={styles.switch}/>
+        <hr className={styles.lineDiviser} />
         <Image
           src="/assets/image-avatar.jpg"
           alt="Image de perfil"
           width={32}
           height={32}
+          className={styles.profile}
         />
       </div>
     </aside>
