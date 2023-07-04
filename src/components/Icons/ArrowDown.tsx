5bdf4e3041c3e40b65352788fdfdc6a68af54866
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-04 16:16:11 -0300

    add novo conteudo svg e comentando o antigo

--- src/components/Icons/ArrowDown.tsx
+++ src/components/Icons/ArrowDown.tsx
@@ -1,6 +1,11 @@
-export default function ArrowDown() {
+export default function ArrowDown({ className }: { className?: string }) {
   return (
-    <svg width="11" height="7" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="11"
+      height="7"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className ? className : undefined}
+    >
       <path
         d="M1 1l4.228 4.228L9.456 1"
         stroke="#7C5DFA"
