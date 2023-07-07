5a03ab07cec597b64f4c47da09aeb3c0a2990289
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-07 16:20:50 -0300

    add styles e hook match media

--- src/components/ContentPageInvoice/index.tsx
+++ src/components/ContentPageInvoice/index.tsx
@@ -6,6 +6,7 @@ import { Item } from "@/types/datas";
 import { formatDate, formatNumber } from "@/utils/utils";
 import Link from "next/link";
 import Status from "../Status";
+import styles from "./styles.module.css";
 
 export default function ContentPageInvoice({
   idInvoice,
@@ -17,85 +18,162 @@ export default function ContentPageInvoice({
     (invoice) => invoice.id === idInvoice
   );
 
+  const buttons = (
+    <div className={styles.headerContainerButtons}>
+      <button
+        type="button"
+        title="Editar Fatura"
+        aria-label="Editar Fatura"
+        className={`btn3 ${styles.btn}`}
+      >
+        Editar
+      </button>
+      <button
+        type="button"
+        title="Excluir Fatura"
+        aria-label="Excluir Fatura"
+        className={`btn5 ${styles.btn}`}
+      >
+        Excluir
+      </button>
+      <button
+        type="button"
+        title="Marcar fatura como paga"
+        aria-label="Marcar fatura como paga"
+        className={`btn2 ${styles.btn}`}
+      >
+        Marcar como pago
+      </button>
+    </div>
+  );
+
+  const footer = useMatchMedia({
+    mobileContent: buttons,
+    desktopContent: null,
+    mediaQuery: "(min-width: 690px)",
+  });
+
+  const header = useMatchMedia({
+    mobileContent: (
+      <header className={styles.header}>
+        <div className={styles.headerContainer}>
+          <p className={styles.headerText}>Status</p>
+          <Status
+            status={
+              invoiceSelected?.status === undefined
+                ? "Sem Status"
+                : invoiceSelected.status
+            }
+          />
+        </div>
+      </header>
+    ),
+    desktopContent: (
+      <header className={styles.header}>
+        <div className={styles.headerContainer}>
+          <p className={styles.headerText}>Status</p>
+          <Status
+            status={
+              invoiceSelected?.status === undefined
+                ? "Sem Status"
+                : invoiceSelected.status
+            }
+          />
+        </div>
+        {buttons}
+      </header>
+    ),
+    mediaQuery: "(min-width: 690px)",
+  });
+
   if (!invoiceSelected) {
     //TODO: arruma o html aqui
-    return "Fatura não encontrada";
+    return <h1>Fatura não encontrada</h1>;
   }
 
   return (
     <>
-      <header>
-        <div>
-          <p>Status</p>
-          <Status status={invoiceSelected.status} />
-        </div>
-        <div>
-          <button
-            type="button"
-            title="Editar Fatura"
-            aria-label="Editar Fatura"
-          >
-            Editar
-          </button>
-          <button
-            type="button"
-            title="Excluir Fatura"
-            aria-label="Excluir Fatura"
-          >
-            Excluir
-          </button>
-          <button
-            type="button"
-            title="Marcar fatura como paga"
-            aria-label="Marcar fatura como paga"
-          >
-            Marcar como pago
-          </button>
-        </div>
-      </header>
-      <section>
-        <div>
+      {header}
+      <section className={styles.section}>
+        <div className={styles.sectionContainerHeader}>
           <div>
-            <b>
-              <span>#</span>
+            <b className={styles.sectionId}>
+              <span className={styles.sectionHash}>#</span>
               {invoiceSelected.id}
             </b>
-            <p>{invoiceSelected.descricao}</p>
+            <p className={styles.sectionLabelInfo}>
+              {invoiceSelected.descricao}
+            </p>
           </div>
-          <address>
-            <span>{invoiceSelected.enderecoRemetente.rua}</span>
-            <span>{invoiceSelected.enderecoRemetente.cidade}</span>
-            <span>{invoiceSelected.enderecoRemetente.codigoPostal}</span>
-            <span>{invoiceSelected.enderecoRemetente.pais}</span>
+          <address
+            className={`${styles.sectionLabelInfo} ${styles.sectionAddres}`}
+          >
+            {invoiceSelected.enderecoRemetente.rua}
+            <br />
+            {invoiceSelected.enderecoRemetente.cidade}
+            <br />
+            {invoiceSelected.enderecoRemetente.codigoPostal}
+            <br />
+            {invoiceSelected.enderecoRemetente.pais}
           </address>
         </div>
-        <div>
-          <div>
-            <div>
-              <p>Data da fatura</p>
-              <b>{formatDate(invoiceSelected.criadoEm)}</b>
+        <div className={styles.sectionContainerBody}>
+          <div className={styles.sectionContainerDatas}>
+            <div className={styles.containerData}>
+              <p
+                className={`${styles.sectionLabelInfo} ${styles.sectionLabelMargin}`}
+              >
+                Data da fatura
+              </p>
+              <b className={styles.sectionValueInfo}>
+                {formatDate(invoiceSelected.criadoEm)}
+              </b>
             </div>
-            <div>
-              <p>Pagamento Vencido</p>
-              <b>{formatDate(invoiceSelected.vencimento)}</b>
+            <div className={styles.containerData}>
+              <p
+                className={`${styles.sectionLabelInfo} ${styles.sectionLabelMargin}`}
+              >
+                Pagamento Vencido
+              </p>
+              <b className={styles.sectionValueInfo}>
+                {formatDate(invoiceSelected.vencimento)}
+              </b>
             </div>
           </div>
           <div>
-            <p>Conta Para</p>
-            <b>{invoiceSelected.nomeCliente}</b>
-            <address>
-              <span>{invoiceSelected.enderecoCliente.rua}</span>
-              <span>{invoiceSelected.enderecoCliente.cidade}</span>
-              <span>{invoiceSelected.enderecoCliente.codigoPostal}</span>
-              <span>{invoiceSelected.enderecoCliente.pais}</span>
+            <p
+              className={`${styles.sectionLabelInfo} ${styles.sectionLabelMargin}`}
+            >
+              Conta Para
+            </p>
+            <b
+              className={`${styles.sectionValueInfo} ${styles.sectionValueMargin}`}
+            >
+              {invoiceSelected.nomeCliente}
+            </b>
+            <address
+              className={`${styles.sectionLabelInfo} ${styles.sectionAddres}`}
+            >
+              {invoiceSelected.enderecoCliente.rua}
+              <br />
+              {invoiceSelected.enderecoCliente.cidade}
+              <br />
+              {invoiceSelected.enderecoCliente.codigoPostal}
+              <br />
+              {invoiceSelected.enderecoCliente.pais}
             </address>
           </div>
           <div>
-            <p>Enviado para</p>
+            <p
+              className={`${styles.sectionLabelInfo} ${styles.sectionLabelMargin}`}
+            >
+              Enviado para
+            </p>
             <Link
               href={`mailto:${invoiceSelected.emailCliente}`}
               target="_blank"
               rel="external"
+              className={`${styles.sectionValueInfo} ${styles.sectionLinkEmail}`}
             >
               {invoiceSelected.emailCliente}
             </Link>
@@ -103,14 +181,19 @@ export default function ContentPageInvoice({
         </div>
         <TableItems items={invoiceSelected.items} />
       </section>
+      {footer}
     </>
   );
 }
 
 function TableItems({ items }: { items: Item[] }) {
   const tableMobile = (
-    <div role="table" aria-label="Itens de cobrança da fatura">
-      <div role="rowgroup">
+    <div
+      role="table"
+      aria-label="Itens de cobrança da fatura"
+      className={styles.table}
+    >
+      <div role="rowgroup" className={styles.tableRowGroupHeader}>
         <div role="row">
           <span role="columnheader">Nome Item</span>
           <span role="columnheader">QTD</span>
@@ -118,25 +201,52 @@ function TableItems({ items }: { items: Item[] }) {
           <span role="columnheader">Total</span>
         </div>
       </div>
-      <div role="rowgroup">
+      <div role="rowgroup" className={styles.tableRowGroupBody}>
         {items.map((item, index) => (
-          <div role="row" key={index}>
+          <div role="row" key={index} className={styles.tableRow}>
             <div aria-hidden="true">
-              <span role="cell">{item.nome}</span>
+              <span
+                role="cell"
+                className={`${styles.tableCell} ${styles.tableCellName}`}
+              >
+                {item.nome}
+              </span>
               <p>
-                <span role="cell">{item.quantidade}</span>
-                <span aria-hidden="true">x</span>
-                <span role="cell">{formatNumber(item.preco)}</span>
+                <span
+                  role="cell"
+                  className={`${styles.tableCell} ${styles.tableCellQtd}`}
+                >
+                  {item.quantidade}
+                </span>{" "}
+                <span
+                  aria-hidden="true"
+                  className={`${styles.tableCell} ${styles.tableDiviser}`}
+                >
+                  x
+                </span>{" "}
+                <span
+                  role="cell"
+                  className={`${styles.tableCell} ${styles.tableCellPreco}`}
+                >
+                  {formatNumber(item.preco)}
+                </span>
               </p>
             </div>
-            <span role="cell">{formatNumber(item.total)}</span>
+            <span
+              role="cell"
+              className={`${styles.tableCell} ${styles.tableCellTotal}`}
+            >
+              {formatNumber(item.total)}
+            </span>
           </div>
         ))}
       </div>
-      <div>
-        <p>
-          <span>Total geral</span>
-          <span>{formatNumber(items.reduce((a, c) => a + c.total, 0))}</span>
+      <div className={styles.tableFooter}>
+        <p className={styles.tableContainerFooter}>
+          <span className={styles.tableTotalGeralCell}>Total geral</span>
+          <span className={styles.tableTotalValue}>
+            {formatNumber(items.reduce((a, c) => a + c.total, 0))}
+          </span>
         </p>
       </div>
     </div>
