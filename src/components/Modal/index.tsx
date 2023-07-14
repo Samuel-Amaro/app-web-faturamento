3289bc8d61a2c837ceee60b10640ad70ee0fd5bf
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-14 16:55:21 -0300

    importando estilos iniciais, do modal, usando componente nativo html, e add novas props, e usando custom hook

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { ReactNode, useEffect, useRef } from "react";
+import styles from "./styles.module.css";
+import useNoScroll from "@/hooks/useNoScroll";
 
-export default function Modal({ children }: { children: ReactNode }) {
+export default function Modal({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   const refDialog = useRef<HTMLDialogElement | null>(null);
 
   useEffect(() => {
@@ -14,5 +22,14 @@ export default function Modal({ children }: { children: ReactNode }) {
     };
   }, [refDialog]);
 
-  return <dialog ref={refDialog}>{children}</dialog>;
+  useNoScroll();
+
+  return (
+    <dialog
+      ref={refDialog}
+      className={className ? `${styles.dialog} ${className}` : styles.dialog}
+    >
+      {children}
+    </dialog>
+  );
 }
