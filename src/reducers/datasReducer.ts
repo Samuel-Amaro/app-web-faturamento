31526f339d9923aef71f8e2f89cc9ba00533b2bf
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-16 10:12:13 -0300

    add action reducer para changed

--- src/reducers/datasReducer.ts
+++ src/reducers/datasReducer.ts
@@ -1,14 +1,27 @@
 import { Fatura } from "@/types/datas";
 
-export type ActionTypeDatasReducer = {
-  type: "save_new_invoice";
-  invoice: Fatura;
-};
+export type ActionTypeDatasReducer =
+  | {
+      type: "save_new_invoice";
+      invoice: Fatura;
+    }
+  | {
+      type: "changed";
+      invoice: Fatura;
+    };
 
 export function datasReducer(draft: Fatura[], action: ActionTypeDatasReducer) {
   switch (action.type) {
     case "save_new_invoice": {
       return [...draft, action.invoice];
+    }
+    case "changed": {
+      return draft.map((invoice) => {
+        if (invoice.id === action.invoice.id) {
+          return action.invoice;
+        }
+        return invoice;
+      });
     }
     default: {
       throw Error("Ação desconhecida Reducer Datas");
