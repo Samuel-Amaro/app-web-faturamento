b105349b68656b3f19b5f4420d662e0660ad3311
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-17 13:28:48 -0300

    refactor componente Modal, aceitando novas props

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,32 +2,41 @@
 
 import { ReactNode, useEffect, useRef } from "react";
 import styles from "./styles.module.css";
-import useNoScroll from "@/hooks/useNoScroll";
 
-export default function Modal({
-  children,
-  className,
-}: {
+interface Props {
   children: ReactNode;
   className?: string;
-}) {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function Modal({ children, className, isOpen, onClose }: Props) {
   const refDialog = useRef<HTMLDialogElement | null>(null);
 
+  function handleClose() {
+    if (refDialog.current) {
+      refDialog.current.close();
+    }
+    onClose();
+  }
+
   useEffect(() => {
-    const dial = refDialog.current;
-    dial?.showModal();
-
-    return () => {
-      dial?.close();
-    };
-  }, [refDialog]);
-
-  useNoScroll();
+    if (refDialog.current) {
+      if (isOpen) {
+        document.body.classList.add("has-dialog");
+        refDialog.current.showModal();
+      } else {
+        refDialog.current.close();
+        document.body.classList.remove("has-dialog");
+      }
+    }
+  }, [isOpen]);
 
   return (
     <dialog
       ref={refDialog}
       className={className ? `${styles.dialog} ${className}` : styles.dialog}
+      onClose={handleClose}
     >
       {children}
     </dialog>
