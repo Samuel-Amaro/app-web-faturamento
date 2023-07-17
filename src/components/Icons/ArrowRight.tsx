902568eaf77248dc4bac8d73c84f381e34d5dd22
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-17 15:36:47 -0300

    novo componente Icon svg

---
+++ src/components/Icons/ArrowRight.tsx
@@ -0,0 +1,13 @@
+export default function ArrowRight({ className }: { className?: string }) {
+  return (
+    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path
+        d="M1 1l4 4-4 4"
+        stroke="#7C5DFA"
+        strokeWidth="2"
+        fill="none"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
