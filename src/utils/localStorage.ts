18d67665e5b95d6defd1b08257d5142678af0649
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-21 09:22:47 -0300

    change, validando se window existe em ambientes de navegador

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -4,20 +4,28 @@ export function getLocalStorage<Type>(
   key: string,
   initialValue: Type | (() => Type)
 ) {
-  const data = window.localStorage.getItem(key);
-
-  if (!data) {
+  if (typeof window !== "undefined") {
+    const data = window.localStorage.getItem(key);
+    if (!data) {
+      if (typeof initialValue === "function") {
+        return (initialValue as () => Type)();
+      } else {
+        return initialValue;
+      }
+    }
+    return JSON.parse(data) as Type;
+  } else {
     if (typeof initialValue === "function") {
       return (initialValue as () => Type)();
     } else {
-        return initialValue;
+      return initialValue;
     }
   }
-
-  return JSON.parse(data) as Type;
 }
 
 export function setLocalStorage(key: string, value: unknown) {
+  if (typeof window !== "undefined") {
     const data = JSON.stringify(value);
     window.localStorage.setItem(key, data);
+  }
 }
